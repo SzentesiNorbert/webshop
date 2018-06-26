b1eab492758112b5c44025477b4040bd2094f220
Szentesi Norbert <szentesinorbert@freemail.hu>
2018-06-26 23:15:05 +0200

    Webshop VUE alap2

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -1,28 +1,252 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <body class="container-fluid">
+      <div id="header">
+      </div>
+    <!--Menü-->
+    <!--
+      <div id="menu-container container-fluid col-lg-auto col-md-auto col-xs-auto">
+        <div class="col-group nav-container col-lg-auto col-md-auto col-xs-auto" role="navigation">
+          <ul id="nav" class="col-group">
+          <li class="col-lg-auto col-md-auto col-sm-auto col-xs-auto">&nbsp;&nbsp;&nbsp;Posters&nbsp;&nbsp;&nbsp;</li>
+          <li class="col-lg-auto col-md-auto col-sm-auto col-xs-auto">&nbsp;Accessory&nbsp;</li>
+          <li class="col-lg-auto col-md-auto col-sm-auto col-xs-auto">&nbsp;&nbsp;&nbsp;&nbsp; T-Shirt &nbsp;&nbsp;&nbsp;&nbsp;</li>
+          <li class="col-lg-auto col-md-auto col-sm-auto col-xs-auto">Steel Cases</li>
+          <li class="col-lg-auto col-md-auto col-sm-auto col-xs-auto">Retro Games</li>
+        </ul>
+        </div>
+      </div>
+    -->
+    <!--Tartalom-->
+
+      <div id="main-content">
+        <br>
+        <br>
+        <p> Poster size: 23x16 <br> Material: paper</p>
+        <hr/>
+      <p>All our posters are shipped within 3 business days in a poster tube wrapped in bubblewrap coating. </p>
+      <h1> Check out my other posters </h1>
+
+
+        <div class="row image-responsive">
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="../pics/1.jpg" width="300" alt="Mario Kart 8"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="../pics/2.jpg" width="300" alt="Mass Effect Andromeda"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="../pics/3.jpg" width="300" alt="Mario Odyssey"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+    <!--
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="pics/4.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+          <div class="item col-lg-auto col-md-auto col-xs-auto">
+            <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+            <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    <div class="item col-lg-auto col-md-auto col-xs-auto">
+      <img src="http://i.ebayimg.com/images/g/LiIAAOSwHjNV8CCe/s-l1600.jpg" width="300" alt="Metal Gear Solid V: The Phantom Pain"><br>
+      <form action="http://www.ebay.com/itm/Original-Metal-Gear-Solid-V-The-Phantom-Pain-23x16-Poster-/111796539639?ssPageName=STRK:MESE:IT" target="_blank"><button class="btn">Buy it Now</a><br></button></form></div>
+
+    </div>
+
+    -->
+    <!--Lábjegyzet-->
+  </div>
+      </div>
+      <div id="footer">
+      </div>
+      </footer>
+
+
+      </body>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css?family=Indie+Flower|Kaushan+Script|Roboto&subset=latin-ext');
+
+* {
+  margin: 0;
+  padding: 0;
+}
+/*fejléc*/
+#main-content {
+
+}
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: red;
+  font-family: 'Kaushan Script';
+  font-size: 2em;
+  padding-top: 1em;
+  text-shadow: 5px 4px 4px black;
+  text-transform: uppercase;
+
+
+}
+p {
+  font-family: cursive;
+  font-size: 2em;
+  text-align: center;
+  font-weight: bold;
+}
+#header{
+  background-color:black;
+  height: 300px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-image:url("../pics/header.jpg");
+  background-repeat: round;
+  background-size: contain;
+  box-shadow: 5px 0 30px 20px white;
+}
+/* navigáció */
+
+
+#menu-container {
+background: #efefef none repeat scroll 0 0;
+}
+
+.nav-container {
+display:flex;
+
+
+
+}
+
+#nav {
+text-align:center;
+}
+
+#menu-container .nav-container .col-group {
+white-space: nowrap;
+
+}
+.col-group {
+width: 100%;
+margin-left: auto;
+margin-right: auto;
+
+}
+ul {
+list-style: outside none none;
+}
+*, *::before, *::after{
+
+  box-sizing: border-box;
+}
+#nav > li {
+text-transform: uppercase;
+display: inline-flex;
+position: relative;
+margin-top: 20px;
+background-image: url("http://www.nuflowwidebay.com.au/wp-content/uploads/2015/10/expences-button-png-hi.png");
+background-position: center;
+background-size: contain;
+background-repeat: no-repeat;
+font-size: 2em;
+background-blend-mode: hue;
+padding:2%;
+}
+
+
+#nav > li > a {
+color: black;
+display:flex;
+font-weight: bold;
+line-height: 50px;
+text-transform: uppercase;
+}
+
+a {
+text-decoration: none;
+
+}
+li {
+  font-family: 'Kaushan Script', cursive;
+  font-family: 'Roboto', sans-serif;
+  background-image: url("http://www.nuflowwidebay.com.au/wp-content/uploads/2015/10/expences-button-png-hi.png");
+}
+
+/*Tartalom*/
+body {
+
+background-image: linear-gradient(to bottom, white 20%, steelblue 50%);
+}
+.row {
+  display: inline-flex;
+  text-align: center;
+  margin-top: 20px;
+}
+.item {
+margin-top: 20px;
+margin-left:auto;
+margin-right: auto;
+
+}
+form {
+margin: 10px;
+
+}
+
+img {
+  border-radius: 35px;
+  overflow: none;
 }
 </style>
