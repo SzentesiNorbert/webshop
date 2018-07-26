7fa7ed5c4d3f1f8c41c23521b384da7c994f06af
Szentesi Norbert <szentesinorbert@freemail.hu>
2018-07-26 08:22:39 +0200

    iframe

--- script.js
+++ script.js
@@ -1,66 +1,23 @@
-paypal.Button.render({
-
-    // Set your environment
-
-    env: 'production', // sandbox | production
-
-    // Specify the style of the button
-
-    style: {
-        label: 'checkout',  // checkout | credit | pay | buynow | generic
-        size:  'responsive', // small | medium | large | responsive
-        shape: 'pill',   // pill | rect
-        color: 'gold'   // gold | blue | silver | black
-    },
-
-    // PayPal Client IDs - replace with your own
-    // Create a PayPal app: https://developer.paypal.com/developer/applications/create
-
-    client: {
-        sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
-        production: '<szentesinorbert@freemail.hu>'
-    },
-
-    // Wait for the PayPal button to be clicked
-
-    payment: function(data, actions) {
-        return actions.payment.create({
-            payment: {
-                transactions: [
-                    {
-                        amount: { total: '1000', currency: 'HUF' }
-                    }
-                ]
-            }
-        });
-    },
-
-    // Wait for the payment to be authorized by the customer
-
-    onAuthorize: function(data, actions) {
-        return actions.payment.execute().then(function() {
-            window.alert('Payment Complete!');
-        });
-    }
-
-}, '#paypal-button-container');
 
 
+/*PayPal */
 
 
-
-
+/* Számláló */
 
 var clicks = 0;
 
 function kosarba() {
  clicks++;
  document.getElementById("clicks").innerHTML = clicks;
+ var cart = clicks;
 
 };
 function kosarbol() {
 if (clicks !== 0 ) {
  clicks--
+
+ var cart = clicks;
 }
 
 document.getElementById("clicks").innerHTML = clicks;
@@ -74,3 +31,11 @@ clicks*1000
 document.getElementById("total").innerHTML = clicks*1000;
 
 };
+
+/*Kosár*/
+
+function kosar(clicks) {
+  return clicks*1000;
+
+}
+/*Kereső */
